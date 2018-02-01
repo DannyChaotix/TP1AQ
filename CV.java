f96480e2cdae017e56ec6b97013e9bd499517a83
Patrick Papineau <patpap002gmail.com>
2018-02-01 13:46:59 -0500

    premier commit

---
+++ CV.java
@@ -0,0 +1,26 @@
+public class CV {
+private String nom;
+private String prenom;
+private String formation;
+private int experiences;
+private String competences;
+private String attentes;
+
+
+	public static void main(String[] args){
+		
+	}
+	
+	public CV(String nom, String prenom, String formation, int experiences,
+			String competences, String attentes) {
+			this.nom = nom;
+			this.prenom = prenom;
+			this.formation = formation;
+			this.experiences = experiences;
+			this.competences = competences;
+			this.attentes = attentes;
+		}
+
+		
+}
+
