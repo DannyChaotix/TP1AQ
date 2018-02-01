7d263226496def5e164ad1b64cbebf2ead61242a
David Provost <dvdp26@gmail.com>
2018-02-01 14:49:26 -0500

    Deuxieme version

--- CV.java
+++ CV.java
@@ -7,9 +7,15 @@ private String competences;
 private String attentes;
 
 
-	public static void main(String[] args){
-		
-	}
+public static void main(String[] args){
+	System.out.println("Bienvenue chez barette!");
+	
+	CV cvP = new CV("Papineau", "Patrick", "Informatique de gestion", 3, "Java", "Assurer la qualité d un logiciel." );
+	CV cvD = new CV("Provost", "David", "Informatique de gastion", 2, "HTML", "Programmer");
+	
+	cvP.affiche();
+	cvD.affiche();
+}
 	
 	public CV(String nom, String prenom, String formation, int experiences,
 			String competences, String attentes) {
@@ -20,7 +26,9 @@ private String attentes;
 			this.competences = competences;
 			this.attentes = attentes;
 		}
-
+	public void affiche(){
+		
+	}
 		
 }
 
