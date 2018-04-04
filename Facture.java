6787abc7da5ab18eb9e9a79bcefd5f2c672d2c1e
Unknown <dvdp26@gmail.com>
2018-04-03 20:49:16 -0400

    Ma version fonctionelle
    il y a des erreures dans l'affichage sur le fichier. Utilise les /n pour separer les lignes

--- Facture.java
+++ Facture.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Facture {
 
 	private String[] tabClients;
-	private String[] tabTables;
+	private int[] tabTables;
 
 	public String[] getTabClients() {
 		return tabClients;
@@ -28,24 +28,25 @@ public class Facture {
 	private double[] tabPrix;
 	private String[] tabCommandes;
 	private String affichage = "";
-	int top = 0;
+	private int top;
 	private static List<String> erreures = new ArrayList<String>();
 
 	public Facture(String[] tabClients, String[] tabPlats, String[] tabCommandes) {
 		super();
-		String[][] clientsTemp = new String[tabClients.length][2];
+		this.tabTables = new int[tabClients.length];
+		this.tabClients = new String[tabTables.length];
 		for (int i = 0; i < tabClients.length; i++) {
 			String[] temp = tabClients[i].split(" ");
-			clientsTemp[i][0] = temp[0];
-			clientsTemp[i][1] = temp[1];
-			int tempTop = Integer.parseInt(temp[1]);
+			this.tabClients[i] = temp[0];
+			tabTables[i] = Integer.parseInt(temp[1]);
+			int tempTop = tabTables[i];
 			if (tempTop > top) {
 				top = tempTop;
+
 			}
 
 		}
-		this.tabClients = clientsTemp[0];
-		this.tabTables = clientsTemp[1];
+
 		this.tabCommandes = tabCommandes;
 		this.tabPrix = new double[tabPlats.length];
 		this.tabPlats = new String[tabPlats.length];
@@ -63,9 +64,9 @@ public class Facture {
 		Double[] tabPrixTables = new Double[top];
 		String[] tabAjout = new String[top];
 		DecimalFormat format = new DecimalFormat("0.00$");
-		for (int i = 1; i < top; i++) {// Les Tables "i"
 
-			tabAjout[i]+= "\tPlats:\n";
+		for (int i = 0; i < top; i++) {// Les Tables "i"
+			tabAjout[i] += "\tPlats:\n";
 			for (int j = 0; j < tabCommandes.length; j++) {// Les Commandes "j"
 				String[] commande = tabCommandes[j].split(" ");
 
@@ -74,23 +75,35 @@ public class Facture {
 					if (commande[0].equals(tabClients[k])) {
 						testUser = true;
 
-						if (i == Integer.parseInt(tabTables[k])) {// Si la commande fait partie de la table
+						if (i + 1 == tabTables[k]) {// Si la commande fait
+													// partie de la table
 
 							boolean testPlat = false;
-							for (int l = 0; l < tabPlats.length; l++) {// Les Plats "l"
+							for (int l = 0; l < tabPlats.length; l++) {// Les
+																		// Plats
+																		// "l"
 
-								if (tabPlats[l].equals(commande[1])) {// Trouve le plat
+								if (tabPlats[l].equals(commande[1])) {// Trouve
+																		// le
+																		// plat
 									testPlat = true;
 									boolean testNum = false;
-									if (commande.length == 3 || Integer.parseInt(commande[2]) >= 1) {// Si la quantitée
-																										// est valide
-
+									if (commande.length == 3 || Integer.parseInt(commande[2]) >= 1) {// Si
+																										// la
+																										// quantitée
+										if (tabPrixTables[i] == null) { // est
+																		// valide
+											tabPrixTables[i] = 0.0;
+										}
 										testNum = true;
-										tabAjout[i] ="\t"+ commande[1] + " * " + commande[2] + "= "
+										tabAjout[i] = "\t" + commande[1] + " * " + commande[2] + "= "
 												+ format.format((tabPrix[l] * Integer.parseInt(commande[2]))) + "\n";
-										tabPrixTables[i] += (tabPrix[l] * Integer.parseInt(commande[2]));// Ajoute le
-																											// prix a la
-																											// table
+										tabPrixTables[i] += (tabPrix[l] * Integer.parseInt(commande[2]));// Ajoute
+																											// le
+										// prix
+										// a
+										// la
+										// table
 									}
 									if (!testNum) {// La quantitée est invalide
 										erreures.add("La quantitée de " + commande[2]);
@@ -112,28 +125,37 @@ public class Facture {
 			}
 
 		}
-
+		int testQuant = 0;
 		for (int m = 0; m < top; m++) {// Les tables (Encore) "m"
 
-			int testQuant = 0;
-			for (int n = 0; n > tabClients.length; n++) {// les Clients (encore) "n"
-
-				if (Integer.parseInt(tabTables[n]) == m) {
+			for (int n = 0; n > tabTables.length; n++) {// les Clients (encore)
+														// "n"
+				if (tabTables[n] == m + 1) {
 					testQuant++;
 				}
-				if (testQuant > 0) {
-					affichage += "Table " + (m + 1) + "\n\n";
-					affichage +=tabAjout[m];
+			}
+			if (testQuant > 0) {
+				affichage += "Table " + (m + 1) + "\n\n";
+				affichage += tabAjout[m];
+			}
+
+			if (tabPrixTables[m] != null) {
+
+				if (tabPrixTables[m] >= 100 || testQuant >= 3) {// le prix est
+																// au dessu de
+																// 100$/plus de
+																// 3 personnes a
+																// la
+																// table
+					tabPrixTables[m] += (tabPrixTables[m] * 0.15);
 				}
+				affichage += "Table" + (m + 1) + "\n\n";
+				affichage += "Prix Brut = " + format.format(tabPrixTables[m]) + "\n";
+				tabPrixTables[m] += tabPrixTables[m] * 0.15;// Taxes
+				affichage += "Ajout des taxes: " + format.format(tabPrixTables[m] * 0.15) + "\n";
+				affichage += "Total: " + format.format(tabPrixTables[m]);
+
 			}
-			if (tabPrixTables[m] >= 100 || testQuant >= 3) {// le prix est au dessu de 100$/plus de 3 personnes a la
-															// table
-				tabPrixTables[m] += (tabPrixTables[m] * 0.15);
-			}
-				affichage +="Prix Brut = " + format.format(tabPrixTables[m]) + "\n";
-			tabPrixTables[m] += tabPrixTables[m] * 0.15;// Taxes
-				affichage += "Ajout des taxes: "+format.format(tabPrixTables[m] * 0.15)+ "\n";
-				affichage += "Total: "+tabPrixTables[m];
 		}
 
 		return affichage;
